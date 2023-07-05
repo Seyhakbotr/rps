7981bb2dba619d864be6163096434e96a69290a9
lotus <blightningboltz5@gmail.com>
2023-07-05 16:07:25 +0700

    initital

--- index.js
+++ index.js
@@ -19,15 +19,47 @@ function getComputerChoice() {
 }
 
 function playRPS(playerSelection,computerSelection) {
-    
+        if (playerSelection === computerSelection){
+            console.log("It's a Tie!");
+        } else if (playerSelection === 'r' && computerSelection === 's'){
+            console.log("You win! Rock beat scissors");
+        } else if (playerSelection === 'p' && computerSelection === 'r'){
+            console.log("You win! Paper beat rock");
+        } else if (playerSelection === 's' && computerSelection === 'p'){
+            console.log("You win! Scissors beat Paper");
+        } else if (computerSelection === 'r' && playerSelection == 's'){
+            console.log("You lose! Paper beat rock");            
+        } else if (computerSelection === 'p' && playerSelection === 'r'){
+            console.log("You lose! Paper beat rock");            
+        } else if (computerSelection === 's' && playerSelection === 's'){
+            console.log("You lose! Paper beat rock");   
+        }
+        return playerSelection, computerSelection;
 }
 function getUserChoice(){
     while(player != 'r' && player != 'p' && player != 's'){
     player = prompt("Please choose one of the following\n 'r' for rock \n 'p' for paper \n 's' for scissors \n");
+    switch (player) {
+        case 'r': return 'r';
+        case 'p': return 'p';
+        case 's': return 's';
+            
+        default:
+            alert("Invalid characters!");
+            break;
+     }
     }
     return player;
 }
-alert(getUserChoice());
+
+function game() {
+    
+}
+
+
 const computerSelection = getComputerChoice();
 const playerSelection = getUserChoice();
+alert(getUserChoice());
+console.log(playRPS(playerSelection,computerSelection));
+
 // console.log(getComputerChoice(num));
