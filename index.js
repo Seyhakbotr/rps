0863778cfb8c4ebe9967a12adccbde9b554cb90a
Frying Lotus <blightningboltz5@gmail.com>
2023-08-20 00:27:29 +0700

    Push to github

--- index.js
+++ index.js
@@ -1,65 +1,79 @@
-let num;
 
-let player
-function getComputerChoice() {
+let computer;
 
-     num = Math.floor(Math.random() * 3) + 1;
-     switch (num) {
-        case 1: return 'r';
-        case 2: return 'p';
-        case 3: return 's';
-            
-        default:
-            alert("Invalid characters!");
-            break;
-     }
-    return num;
-    
+function getPlayerChoice()
+{
+    let player;
+    do{
+        player = prompt("Please enter: Rock,Paper,Scissors").toLowerCase();
 
-}
-
-function playRPS(playerSelection,computerSelection) {
-        if (playerSelection === computerSelection){
-            console.log("It's a Tie!");
-        } else if (playerSelection === 'r' && computerSelection === 's'){
-            console.log("You win! Rock beat scissors");
-        } else if (playerSelection === 'p' && computerSelection === 'r'){
-            console.log("You win! Paper beat rock");
-        } else if (playerSelection === 's' && computerSelection === 'p'){
-            console.log("You win! Scissors beat Paper");
-        } else if (computerSelection === 'r' && playerSelection == 's'){
-            console.log("You lose! Paper beat rock");            
-        } else if (computerSelection === 'p' && playerSelection === 'r'){
-            console.log("You lose! Paper beat rock");            
-        } else if (computerSelection === 's' && playerSelection === 's'){
-            console.log("You lose! Paper beat rock");   
-        }
-        return playerSelection, computerSelection;
-}
-function getUserChoice(){
-    while(player != 'r' && player != 'p' && player != 's'){
-    player = prompt("Please choose one of the following\n 'r' for rock \n 'p' for paper \n 's' for scissors \n");
-    switch (player) {
-        case 'r': return 'r';
-        case 'p': return 'p';
-        case 's': return 's';
-            
-        default:
-            alert("Invalid characters!");
-            break;
-     }
-    }
+    }while(player != "rock" && player != "paper" && player != "scissors");
     return player;
-}
+}   
 
-function game() {
+function getComputerChoice(){
+    let computer = Math.floor(Math.random()*3) + 1;
+    switch (computer) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+    }
+}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+        return "tie";
+    } else if(playerSelection == "paper" && computerSelection == "rock") {
+        console.log(`You win! ${playerSelection} beat ${computerSelection}`);
+        return "player";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log(`You win!${playerSelection} beat ${computerSelection}`);
+        return "player";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log(`You win!${playerSelection} beat ${computerSelection}`);
+        return "player";
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        console.log(`You lose!${computerSelection} beat ${playerSelection}`);
+        return "computer";
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        console.log(`You lose!${computerSelection} beat ${playerSelection}`);
+        return "computer";
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        console.log(`You lose!${computerSelection} beat ${playerSelection}`);
+        return "computer";
+    }
+
+}
+function printResult()
+{
+    let playerScore = 0;
+    let computerScore = 0;
+    while (playerScore < 5 && computerScore < 5)
+    {
+        let playerChoice = getPlayerChoice();
+        let computerChoice = getComputerChoice();
+        let result = playRound(playerChoice,computerChoice);
+        if (result == "player")
+        {
+            playerScore++;
+        } else if (result == "computer")
+        {
+            computerScore++;
+        }
+    }
     
+    if (playerScore == 5)
+    {
+        console.log(`Player Win!`);
+    } else if (computerScore == 5)
+    {
+        console.log(`Computer Win!`);
+    } else {
+        console.log(`It's a tie!`);
+    }
 }
-
-
-const computerSelection = getComputerChoice();
-const playerSelection = getUserChoice();
-alert(getUserChoice());
-console.log(playRPS(playerSelection,computerSelection));
-
-// console.log(getComputerChoice(num));
+printResult();
+// test
